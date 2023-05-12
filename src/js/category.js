65ffa8aeefeed39545246f08bdbdeb61bc12ec98
Maksym Karalash <Maks.karalash@gmail.com>
2023-05-12 12:54:06 +0300

    Fix class

--- src/js/category.js
+++ src/js/category.js
@@ -3,15 +3,16 @@ const LINK_All_CATEGORY =
 const LINK_ONE_CATEGORY =
   'https://books-backend.p.goit.global/books/category?category=';
 
-const categoryEl = document.querySelector('.js-list-category');
+const categoryEl = document.querySelector('.js-list-categories');
 const booksEl = document.querySelector('.js-all-books');
+const categoryNameEl = document.querySelector('.js-category-name');
 
 fetch(LINK_All_CATEGORY)
   .then(resp => resp.json())
   .then(data => {
     const sortArr = data.sort((a, b) => a.list_name.localeCompare(b.list_name));
     const arr = sortArr
-      .map(({ list_name }) => `<li class="list">${list_name}</li>`)
+      .map(({ list_name }) => `<li class="categories__item">${list_name}</li>`)
       .join('');
     categoryEl.insertAdjacentHTML('beforeend', arr);
   });
@@ -19,7 +20,7 @@ fetch(LINK_All_CATEGORY)
 categoryEl.addEventListener('click', onClick);
 
 function onClick(e) {
-  if (e.target.classList[0] !== 'list') {
+  if (e.target.classList[0] !== 'categories__item') {
     return;
   }
   const categoryName = e.target.textContent.split(' ');
@@ -27,17 +28,18 @@ function onClick(e) {
     .splice(0, categoryName.length - 1)
     .join(' ');
   const categoryNameTwo = categoryName.at(-1);
-  booksEl.insertAdjacentHTML(
-    'beforebegin',
-    `<h2 class="category_name">${categoryNameOne}
-      <span class="category_name color_violet">${categoryNameTwo}
-      </span>
-    </h2>`
+
+  console.dir(categoryNameEl);
+  categoryNameEl.textContent = categoryNameOne + ' ';
+  categoryNameEl.insertAdjacentHTML(
+    'beforeend',
+    `<span class="category_name category__name--violet">${categoryNameTwo}</span>`
   );
+
   categoryEl
-    .querySelector('.js-current-category')
-    .classList.remove('js-current-category');
-  e.target.classList.add('js-current-category');
+    .querySelector('.categories__current')
+    .classList.remove('categories__current');
+  e.target.classList.add('categories__current');
   fetch(LINK_ONE_CATEGORY + e.target.textContent)
     .then(resp => resp.json())
     .then(data => {
@@ -49,10 +51,10 @@ function markupBooks(data) {
   return data
     .map(
       ({ author, book_image, title }) => `
-  <li class="book">
-    <img class="book__img" src="${book_image}" alt="${title}">
-    <h2 class="book__title">${title}</h2>
-    <h3 class="book__subtitle">${author}</h3>
+  <li class="category__book">
+    <img class="category__img" src="${book_image}" alt="${title}">
+    <h2 class="category__title">${title}</h2>
+    <h3 class="category__subtitle">${author}</h3>
   </li>
   `
     )
