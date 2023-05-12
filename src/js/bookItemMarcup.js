8c4391f8b6411f83133ae190e2f99294fdcc282a
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-05-13 01:30:19 +0300

    Додано id книги як дата атрибут в розмітку <li>

--- src/js/bookItemMarcup.js
+++ src/js/bookItemMarcup.js
@@ -1,6 +1,6 @@
-const bookItemMarcup = ({ author, book_image, title }) => {
+const bookItemMarcup = ({ author, book_image, title, _id }) => {
   return `
-  <li class="category__book">
+  <li class="category__book" data-id=${_id}>
     <img class="category__img" src="${book_image}" alt="${title}" width="180">
     <h2 class="category__title">${title}</h2>
     <h3 class="category__subtitle">${author}</h3>
