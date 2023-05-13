694770c3c1d589db6d4ca36ac92c1da378d3dbff
Yarzik7 <116737021+Yarzik7@users.noreply.github.com>
2023-05-13 16:51:07 +0000

    Deploying to gh-pages from @ Yarzik7/project-DreamTeam@b33c1516b9a9fbe60db4979a5d3be1c370da45ef 🚀

---
+++ scrollup.7f9a4f33.js
@@ -0,0 +1,2 @@
+("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequired7c6.register("2qRC4",(function(e,o){const d=document.querySelector(".btn-scroll");d&&d.classList.add("is-hidden-up"),d&&d.addEventListener("click",(function(e){window.scrollTo({top:0,behavior:"smooth"})})),window.addEventListener("scroll",(function(){window.scrollY>200?d.classList.remove("is-hidden-up"):0==window.scrollY&&d.classList.add("is-hidden-up")}))}));
+//# sourceMappingURL=scrollup.7f9a4f33.js.map
