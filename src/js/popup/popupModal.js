7f173569fe7d07ea32e8b0c37d70b1ebdc3311f0
Alla <urt2509@gmail.com>
2023-05-14 17:33:51 +0200

    create markup, add event list for rendering, close on click

--- src/js/popup/popupModal.js
+++ src/js/popup/popupModal.js
@@ -1,9 +1,9 @@
 import { AxiosApi } from '../api/fetchBooks';
+import saveStorageHandler from './localStorageHandler';
 
 const axiosApi = new AxiosApi();
-
-let modalCard = document.querySelector('.js-popup');
-
+const LOCALSTORAGE_KEY = 'books';
+const popupModal = document.createElement('div');
 const chosenBook = document.querySelector('.js-all-books');
 
 chosenBook.addEventListener('click', onClick);
@@ -11,49 +11,84 @@ chosenBook.addEventListener('click', onClick);
 async function onClick(e) {
   const idBook = e.target.parentElement.dataset.id;
 
-  console.log(idBook);
-
   try {
     const responce = await axiosApi.getShops(idBook);
-    const arr = responce.data;
-    console.log(arr);
-    const mark = createPopupCard(arr);
+    const nodeEl = responce.data;
 
-    modalCard.insertAdjacentHTML('beforeend', mark.join(''));
+    popupModal.innerHTML = createPopupCard(nodeEl);
+    document.body.appendChild(popupModal);
+
+    const addHandler = popupModal.querySelector('.js-add-storage');
+
+    addHandler.addEventListener('click', addToCart(e, nodeEl));
+
+    const btnPopupClose = popupModal.querySelector('.js-popup-close');
+
+    btnPopupClose.addEventListener('click', onClose);
   } catch (error) {
     console.log(error);
   }
 }
 
-function createPopupCard(arr) {
-  arr.map(
-    ({ author, book_image, title, buy_links: [{ name, url }], list_name }) =>
-      `
-    <button  class="js-popup-close" type="button">
-        <svg width="20" height="20"></svg>
-        <use href=""></use>
+function createPopupCard({
+  author,
+  book_image,
+  title,
+  buy_links,
+  list_name,
+  description,
+}) {
+  return `<div class="popup-backdrop">
+  <div class="js-popup popup__body">
+  <div class="popup__container">
+  <button  class="js-popup-close btn__close" type="button">
+        <svg width="16" height="15"></svg>
+        <use href="./images/sprite.svg#icon-x-close"></use>
         </svg>
     </button>
-    <div>
-        <a href="">
-            <img src="${book_image}" alt="${title}">
-        </a>
+    <div class="popup-book">
+    <div class="popup-book__thumb">
+        <img class="popup-book__img" src="${book_image}" alt="${title}">
+        </div>
+    <div class="popup-book__info"
         <h2>${title}</h2>
         <h3>${author}</h3>
-        <p>${list_name}</p>
-        <ul>
-            <li>
-                <a href="${url}">
-                    <img src="" alt="${name}">
-                </a>
-            </li>
-        </ul>
+        <p>${description}</p>
+        <ul class="modal__shops-link">${createShopLinks(buy_links)}</ul>
+        </div>
     </div>
     <div>
-        <button type="button">
+        <button type="button" class="action-button js-add-storage">add to shoping list</button>
+        <p class="popup__text">text</p>
+    </div>
+    </div>
+    </div>
+    </div>`;
+}
 
-        </button>
-        <p class=""></p>
-    </div>`
-  );
+function createShopLinks(buyLinks) {
+  const markupShops = buyLinks
+    .map(
+      ({ name, url }) => `  <li>
+                <a href="${url}" target="_blank" rel="noopener noreferrer nofollow">
+                    <img src="" alt="${name}">
+                </a>
+            </li>`
+    )
+    .join('');
+  return markupShops;
+}
+
+function addToCart(e, value) {
+  console.log(value);
+
+  saveStorageHandler(LOCALSTORAGE_KEY, value);
+}
+
+function onClose(evt) {
+  if (!evt.target) {
+    return;
+  } else {
+    document.body.removeChild(popupModal);
+  }
 }
