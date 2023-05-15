b6d66015a32c32814a71db66b9139025c67ec81b
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-05-15 17:20:06 +0300

    Add hover and refactoring topBooks api

--- src/js/api/topBooks.js
+++ src/js/api/topBooks.js
@@ -1,5 +1,6 @@
 import { getRequest } from '../api/getRequest';
-import { createItemMarkup } from '../createItemMarkup';
+import { topBooksContainerMarcup } from '../topBooksContainerMarcup';
+import { showLoader, hideLoader } from '../loader';
 
 const REFS = {
   ul: document.querySelector('.js-all-books'),
@@ -8,19 +9,19 @@ const REFS = {
 const URL_TOP_BOOKS = `https://books-backend.p.goit.global/books/top-books?`;
 
 async function getBooks() {
-  const BOOKS = await getRequest(`${URL_TOP_BOOKS}`);
+    try {
+      showLoader();
+      const BOOKS = await getRequest(`${URL_TOP_BOOKS}`);
 
-  const ARRAY = BOOKS.map(({ list_name, books }) => {
-    return `
-      <li class="category-top-books">
-        <h3 class="category-top-books__title">${list_name}</h3>
-        <ul class="category-top-books__list">${createItemMarkup(books)}</ul>
-        <button class="action-button category-top-books__action-button" data-categoryName="${list_name}">See more</button>
-      </li>`;
-  }).join('');
+      const ARRAY = BOOKS.map(topBooksContainerMarcup).join('');
 
-  REFS.ul.classList.remove('category__list');
-  REFS.ul.innerHTML = ARRAY;
+      REFS.ul.classList.remove('category__list');
+      REFS.ul.innerHTML = ARRAY;
+    } catch (error) {
+      console.log(error);
+    } finally {
+      hideLoader();
+    }
 }
 
 getBooks();
