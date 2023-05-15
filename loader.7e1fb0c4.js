b56ef3eb1c4051dccf5bfa0883a9f95d7533620a
Yarzik7 <116737021+Yarzik7@users.noreply.github.com>
2023-05-15 08:22:45 +0000

    Deploying to gh-pages from @ Yarzik7/project-DreamTeam@5e7ba9565e92ca7b8a8cf4875bf447a2e3aebdd3 🚀

---
+++ loader.7e1fb0c4.js
@@ -0,0 +1,2 @@
+("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequired7c6.register("kvC6y",(function(e,d){document.getElementById("loader")}));
+//# sourceMappingURL=loader.7e1fb0c4.js.map
