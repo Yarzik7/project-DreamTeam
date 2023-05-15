894d9197f9a3bf6c10fea25a7eb35c0bab91e12e
Sergey Kuran <kuran.serg@gmail.com>
2023-05-15 20:09:05 +0300

    fixed

--- src/js/api/topBooks.js
+++ src/js/api/topBooks.js
@@ -2,26 +2,26 @@ import { getRequest } from '../api/getRequest';
 import { topBooksContainerMarcup } from '../topBooksContainerMarcup';
 import { showLoader, hideLoader } from '../loader';
 
-const REFS = {
+const refs = {
   ul: document.querySelector('.js-all-books'),
 };
 
 const URL_TOP_BOOKS = `https://books-backend.p.goit.global/books/top-books?`;
 
 async function getBooks() {
-    try {
-      showLoader();
-      const BOOKS = await getRequest(`${URL_TOP_BOOKS}`);
+  try {
+    showLoader();
+    const books = await getRequest(`${URL_TOP_BOOKS}`);
 
-      const ARRAY = BOOKS.map(topBooksContainerMarcup).join('');
+    const array = books.map(topBooksContainerMarcup).join('');
 
-      REFS.ul.classList.remove('category__list');
-      REFS.ul.innerHTML = ARRAY;
-    } catch (error) {
-      console.log(error);
-    } finally {
-      hideLoader();
-    }
+    refs.ul.classList.remove('category__list');
+    refs.ul.innerHTML = array;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    hideLoader();
+  }
 }
 
 getBooks();
