5a5e09fe3bb7b6bd938defbadbddbf38fed0b583
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-05-16 10:08:19 +0300

    upd popup

--- src/js/testFiles/imagesTest.js
+++ src/js/testFiles/imagesTest.js
@@ -1,4 +1,14 @@
-import razom from '../../images/razom.png';
-import logoI from '../../images/logo.png';
+import amazon1x from '../../images/shoplist/amazon1x.png';
+import amazon2x from '../../images/shoplist/amazon2x.png';
+import apple1x from '../../images/shoplist/apple1x.png';
+import apple2x from '../../images/shoplist/apple2x.png';
+import bookshop1x from '../../images/shoplist/bookshop1x.png';
+import bookshop2x from '../../images/shoplist/bookshop2x.png';
 
-export { razom, logoI };
+const bookShopsIcons = [
+  { x1: amazon1x, x2: amazon2x, width: '62', height: '19' },
+  { x1: apple1x, x1: apple2x, width: '33', height: '32' },
+  { x1: bookshop1x, x1: bookshop2x, width: '38', height: '36' },
+];
+
+export { bookShopsIcons };
