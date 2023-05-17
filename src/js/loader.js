03179aad0f0e8cf71535176dda04fcf891babb95
Anastasiia262 <gribskay2@gmail.com>
2023-05-17 21:00:48 +0300

    fix book
    fix book

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,11 +1,33 @@
-const loader = document.getElementById("loader");
+class Loader {
+  constructor(element) {
+    this.element = element;
+    this.startAnimation();
+  }
 
-function showLoader() {
-  loader.style.display = "block";
+  startAnimation() {
+    const loaderElement = this.element.querySelector('.loader');
+    const spans = loaderElement.querySelectorAll('span');
+
+    let rotation = 0;
+    let opacity = 1;
+
+    const animation = () => {
+      rotation += 4;
+      opacity -= 0.01;
+
+      loaderElement.style.transform = `rotate(${rotation}deg)`;
+      spans.forEach((span) => {
+        span.style.opacity = opacity;
+      });
+
+      if (opacity <= 0) {
+        clearInterval(interval);
+        loaderElement.remove();
+      }
+    };
+
+    const interval = setInterval(animation, 10);
+  }
 }
 
-function hideLoader() {
-  loader.style.display = "none";
-}
-
-export {showLoader, hideLoader}
+const loader = new Loader(document.querySelector('.loader-container'));
