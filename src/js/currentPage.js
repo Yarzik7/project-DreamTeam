6e7012b1ae82bcf7f6240542ba7093b03edd4d52
Valeria Otchenash <kratorchestra@gmail.com>
2023-05-18 10:51:29 +0300

    Update currentPage.js

--- src/js/currentPage.js
+++ src/js/currentPage.js
@@ -1,16 +1,32 @@
 
-window.onload = function () {
-    const currentPath = window.location.pathname;
+// window.onload = function () {
+//     const currentPath = window.location.pathname;
 
-    const menuItems = document.querySelectorAll('.nav__link, .burger-menu__link');
+//     const menuItems = document.querySelectorAll('.nav__link, .burger-menu__link');
 
-    for (let i = 0; i < menuItems.length; i++) {
-        if (menuItems[i].getAttribute('href') === currentPath) {
-            menuItems[i].classList.add('current-page');
-        } else {
-            menuItems[i].classList.remove('current-page');
+//     for (let i = 0; i < menuItems.length; i++) {
+//         if (menuItems[i].getAttribute('href') === currentPath) {
+//             menuItems[i].classList.add('current-page');
+//         } else {
+//             menuItems[i].classList.remove('current-page');
         
-        }
+//         }
 
+//     }
+// }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const path = window.location.pathname;
+
+    const homeLink = document.querySelector('.nav__link[href="./index.html"]');
+    console.log(homeLink);
+    const shoppingListLink = document.querySelector('.nav__link[href="./shopping-list.html"]');
+
+    if (path === '/index.html' || path === '/') {
+        homeLink.classList.add('current-page');
+        shoppingListLink.classList.remove('current-page');
+    } else if (path === '/shopping-list.html') {
+        homeLink.classList.remove('current-page');
+        shoppingListLink.classList.add('current-page');
     }
-}
+});
