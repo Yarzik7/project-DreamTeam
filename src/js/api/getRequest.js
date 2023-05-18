af1ab4f61d683b312dc17db93e3aa6e1564ccdbc
Sergey Kuran <kuran.serg@gmail.com>
2023-05-18 15:52:46 +0300

    fix

--- src/js/api/getRequest.js
+++ src/js/api/getRequest.js
@@ -7,13 +7,13 @@ import axios from 'axios';
  */
 
 async function getRequest(link) {
-  // try {
-  const response = await axios.get(`${link}`);
-  const data = await response.data;
-  return data;
-  // } catch (err) {
-  // console.log(err);
-  // }
+  try {
+    const response = await axios.get(`${link}`);
+    const data = await response.data;
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export { getRequest };
